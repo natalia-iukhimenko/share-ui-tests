41d20c947468e937ae5c84d8e54d53cab4289e40
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-05-22 22:02:10 +0300

    Fixed "joinForFree" link selector

--- cypress/support/pages/country_home_page.js
+++ cypress/support/pages/country_home_page.js
@@ -1,6 +1,6 @@
 class CountryHomePage {
     get joinForFreeLink() {
-        return cy.get("[data-test-id = 'reggie-link-register-now']").eq(0);
+        return cy.get("[data-test-id = 'reggie-link-register-now']").eq(1);
     }
 
     joinForFree() {
