65a5f0cd4caa5f61cb4d85bf29de62ac19e7c16d
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-05-24 11:20:55 +0300

    Added second test

--- cypress/tests/registration.js
+++ cypress/tests/registration.js
@@ -1,6 +1,9 @@
 import internationalPage from "../support/pages/international_page"
 import countryHomePage from "../support/pages/country_home_page"
 import registrationPage from "../support/pages/registration_page";
+import paymentPage from "../support/pages/payment_page";
+import * as dataGenerator from "../support/util/data_generator";
+const { faker } = require('@faker-js/faker');
 
 describe("UI tests for registration process", () => {
     let countryInfo;
@@ -28,5 +31,22 @@ describe("UI tests for registration process", () => {
             .checkPageHasLink(countryInfo.imprintLink)
             .checkPageHasLink(countryInfo.privacyPolicyLink)
             .checkPageHasLink(countryInfo.cookiesLink);
+    })
+
+    it("Successful registration with only required fields specified", () => {
+        registrationPage.selectCity(countryInfo.cities[0])
+            .setCredentials(dataGenerator.generateEmail(), dataGenerator.generatePassword(), dataGenerator.generatePin())
+            .selectSalutation(1)
+            .setFullName(faker.name.firstName(), faker.name.lastName())
+            .selectBirthDate(1,1, 1)
+            .setBirthPlace(faker.address.country())
+            .setStreet(faker.address.streetName())
+            .setZipCode(faker.address.zipCode())
+            .setCity(faker.address.cityName())
+            .setMobilePhoneNumber(dataGenerator.generatePhoneNumber())
+            .tickTermsAndConditionsCheckbox()
+            .doRegister();
+            paymentPage.checkUrl()
+                .checkHeaderDisplayed();
     })
 })
